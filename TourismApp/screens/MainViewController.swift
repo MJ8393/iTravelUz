2b6f3f6c8375b934e00daa159273e8e46356b776
Mekhriddin Jumaev <jumaevmekhriddin10@gmail.com>
2023-08-12 14:56:04 +0500

    start

---
+++ TourismApp/screens/MainViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MainViewControllet.swift
+//  TourismApp
+//
+//  Created by Mekhriddin Jumaev on 12/08/23.
+//
+
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController {
+    
+    lazy var subView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Main"
+        label.textColor = UIColor.red
+        return label
+    }()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        initViews()
+    }
+    
+    private func initViews() {
+        view.addSubview(subView)
+        subView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        subView.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+
+}
